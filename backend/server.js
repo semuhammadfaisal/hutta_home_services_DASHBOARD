cb18995ac3a783d9600ff69335b0b8543280932c
faisalfreelance4-ops <faisalfreelance4@gmail.com>
2026-02-02 20:47:32 +0500

    first commit

--- backend/server.js
+++ backend/server.js
@@ -34,11 +34,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-module.exports = app;
-
-if (require.main === module) {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
